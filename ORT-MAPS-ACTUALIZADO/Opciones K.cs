7fd46e005838f83030737239c72f1d8db929c1ea
martupirolo <111439509+martupirolo@users.noreply.github.com>
2022-10-21 12:23:02 -0300

    cambios
    cambios

---
+++ ORT-MAPS-ACTUALIZADO/Opciones K.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ORTmaps.Apps
+{
+    public partial class Opciones_K : Form
+    {
+        public Opciones_K()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            Form Pantalla_Kiosco_esquina = new Pantalla_Kiosco_esquina();
+            Pantalla_Kiosco_esquina.Show();
+            this.Hide();
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            Form Kiosco_Simona = new Kiosco_Simona();
+            Kiosco_Simona.Show();
+            this.Hide();
+        }
+
+        private void Button3_Click(object sender, EventArgs e)
+        {
+            Form Supermercado_Coto = new Supermercado_Coto();
+            Supermercado_Coto.Show();
+            this.Hide();
+        }
+
+        private void Button4_Click(object sender, EventArgs e)
+        {
+            Form MapaExterior = new MapaExterior();
+            MapaExterior.Show();
+            this.Hide();
+        }
+    }
+}
---
+++ ORT-MAPS-ACTUALIZADO/OpcionesL.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ORTmaps.Apps
+{
+    public partial class Opciones_L : Form
+    {
+        public Opciones_L()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click_1(object sender, EventArgs e)
+        {
+            Form Librería_JLZ = new Librería_JLZ();
+            Librería_JLZ.Show();
+            this.Hide();
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            Form Forum_libreria = new Forum_libreria();
+            Forum_libreria.Show();
+            this.Hide();
+        }
+
+        private void Button3_Click(object sender, EventArgs e)
+        {
+            Form Lunaria_Libros = new Lunaria_Libros();
+            Lunaria_Libros.Show();
+            this.Hide();
+        }
+
+        private void Button4_Click(object sender, EventArgs e)
+        {
+            Form MapaExterior = new MapaExterior();
+            MapaExterior.Show();
+            this.Hide();
+        }
+    }
+}
